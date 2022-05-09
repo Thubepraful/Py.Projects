18d27a8d2d3ea1ca452ba5cccc1bf47c4e4d4e8c
Praful Thube <99529435+Thubepraful@users.noreply.github.com>
2022-05-09 18:34:19 +0530

    Add files via upload

---
+++ Basic web application/myapp.py
@@ -0,0 +1,63 @@
+from flask import *
+from DBM import *
+
+app=Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/reg")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/addEmp", methods=["POST"])
+def add_emp():
+    ids = request.form["id"]
+    name = request.form["name"]
+    contact = request.form["cont"]
+    email = request.form["email"]
+    passw = request.form["passw"] 
+    t= (ids, name, contact, email, passw)
+    addEmp(t)
+    return redirect("/")
+
+@app.route("/emplist")
+def emp_list():
+    data = selectAllEmp()
+    return render_template("emplist.html", elist=data)
+
+@app.route("/deleteEmp")
+def delete_emp():
+    ids=request.args.get("id")
+    deleteEmp(ids)
+    return redirect("emplist")
+
+@app.route("/editEmp")
+def edit_emp():
+    ids = request.args.get("id")
+    data=selectEmpById(ids)
+    return render_template("update.html",row=data)
+
+@app.route("/updateEmp", methods=["POST"])
+def update_emp():
+    ids = request.form["id"]
+    name = request.form["name"]
+    contact = request.form["cont"]
+    email = request.form["email"]
+    passw = request.form["passw"] 
+    t= (name, contact, email, passw, ids)
+    updateEmp(t)
+    return redirect("emplist")
+
+##@app.route("/loginpage", methods="[POST]")
+##def login():
+##    uname=request.form["username"]
+##    passw=request.form["password"]
+##    t=(uname,passw)
+    
+
+if (__name__=="__main__"):
+    app.run(debug=True)
